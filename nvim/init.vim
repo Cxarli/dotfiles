05b8f76190a795c4380b4d3632778de9afb23697
Cxarli <10348289+Cxarli@users.noreply.github.com>
2022-02-02 23:20:30 +0100

    move from vimscript to nvim lua

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,58 +1,4 @@
-call plug#begin()
-Plug 'neovim/nvim-lspconfig'
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'dense-analysis/ale'
-call plug#end()
-
 lua require('init')
-
-let mapleader = ","
-
-" Modern terminal
-set encoding=utf-8
-set fileencoding=utf-8
-set ttyfast
-set mouse=a
-set fixendofline
-set updatetime=1000
-
-" Syntax highlighting
-syntax on
-
-" Enable history
-set history=10000
-set undodir=$HOME/.nvim/undo
-set undofile
-set undolevels=1000000
-set undoreload=1000000
-set viminfo='10000,%10000,s1024,c,f1,h,r/tmp,n~/.nvim/viminfo
-
-" Theming
-set t_Co=256
-set background=dark
-set showcmd
-set showmatch
-set number
-set laststatus=2
-
-set title
-set colorcolumn=80,100,120
-highlight ColorColumn ctermbg=lightgrey
-
-" Better searching
-set hlsearch
-set incsearch
-set smartcase
-
-" Show whitespace characters
-set list
-set listchars=tab:\|_,trail:~,extends:>,precedes:<
-
-" Configure whitespace usage
-set softtabstop=2
-set shiftwidth=2
-set tabstop=2
-set noexpandtab
 
 " Commands
 nnoremap <silent> <leader><CR> :nohl<CR>
@@ -61,9 +7,9 @@ nnoremap <silent> NN o<ESC>
 
 " Jump back to last position
 au BufReadPost *
-       \ if line("'\"") > 1 && line("'\"") <= line("$")
-       \| exe "normal! g`\""
-       \| endif
+	\ if line("'\"") > 1 && line("'\"") <= line("$")
+	\| exe "normal! g`\""
+	\| endif
 
 " Global yank/paste
 vnoremap <silent> <leader>y :w! /tmp/nvim<CR>
--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,6 +1,65 @@
-require('lualine').setup({
+vim.g.mapleader = ','
+
+-- Modern terminal
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+vim.opt.ttyfast = true
+vim.opt.mouse = 'a'
+vim.opt.fixendofline = true
+vim.opt.updatetime = 100
+
+-- Enable history
+vim.opt.history = 10000
+vim.opt.undodir = vim.env.HOME .. '/.nvim/undo'
+vim.opt.undofile = true
+vim.opt.undolevels = 1000000
+vim.opt.undoreload = 1000000
+vim.opt.viminfo = "'10000,%10000,s1024,c,f1,h,r/tmp,n~/.nvim/viminfo"
+
+-- Theming
+vim.opt.background = 'dark'
+vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.number = true
+vim.opt.laststatus = 2
+
+vim.opt.title = true
+vim.opt.colorcolumn = { 80, 100, 120 }
+vim.cmd [[
+	syntax on
+	highlight ColorColumn ctermbg=lightgrey
+]]
+
+-- Better searching
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrapscan = true
+
+-- Show whitespace characters
+vim.opt.list = true
+vim.opt.listchars = { tab = '|_', trail = '~', extends = '>', precedes = '<' }
+
+-- Configure whitespace usage
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = false
+
+-- Plugins
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin')
+-- Plug 'neovim/nvim-lspconfig'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'dense-analysis/ale'
+vim.call('plug#end')
+
+require('lualine').setup {
 	options = {
 		icons_enabled = false,
 		theme = "powerline",
 	}
-})
+}
+
